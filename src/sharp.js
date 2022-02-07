f51f0f9d64b5c36c38d006add90bc2e4654decb0
Tommy Oldfield <tco1@st-andrews.ac.uk>
2022-02-07 20:22:05 +0000

    FEAT aws s3 upload script

--- src/sharp.js
+++ src/sharp.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
+
 const sharp = require('sharp');
 const path = require('path');
 const fs = require( 'fs' );
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
 
 const moveFrom = path.resolve(__dirname, "images");
 const moveTo = path.resolve(__dirname, "../images");
@@ -34,22 +43,27 @@ const moveTo = path.resolve(__dirname, "../images");
                 .toFile(path.join(fromNoExt.dir, fromNoExt.name) + `.avif`)
                 .then(info => { 
                     console.log(path.join(fromNoExt.dir, fromNoExt.name) + `.avif created`);
+                    uploadFile(fromNoExt, 'avif');
                 })
                 .catch(err => { });
+
 
                 await sharp(fromPath)
                 .webp({})
                 .toFile(path.join(fromNoExt.dir, fromNoExt.name) + `.webp`)
                 .then(info => {
                     console.log(path.join(fromNoExt.dir, fromNoExt.name) + `.webp created`);
+                    uploadFile(fromNoExt, 'webp');
                 })
                 .catch(err => { });
+
 
                 await sharp(fromPath)
                 .jpeg({ mozjpeg: true })
                 .toFile(path.join(fromNoExt.dir, fromNoExt.name) + `.jpeg`)
                 .then(info => {
                     console.log(path.join(fromNoExt.dir, fromNoExt.name) + `.jpeg created`);
+                    uploadFile(fromNoExt, 'jpeg');
                 })
                 .catch(err => { });
 
@@ -76,3 +90,24 @@ const moveTo = path.resolve(__dirname, "../images");
     }
 
 })(); // Wrap in parenthesis and call now
+
+const uploadFile = (fileName, type) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(path.join(fileName.dir, fileName.name) + '.' + type);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: `${fileName.name}.${type}`, // File name you want to save as in S3
+        Body: fileContent,
+        ContentType: `image/${type}`,
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
